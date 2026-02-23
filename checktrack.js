ac9e8395d1a2d9e50042c12846cb98882a090461
Guewen <guewenmalary779@gmail.com>
2026-02-23 15:02:57 +0100

    étape 2

--- checktrack.js
+++ checktrack.js
@@ -1,5 +1,18 @@
+import { homedir } from "os";
 import { existsSync, readFileSync } from "fs";
+import { join } from "path";
 
-const data = readFileSync("track.json", "utf-8");
-const parsedData = JSON.parse(data);
-console.log(parsedData);
+const homePath = homedir();
+console.log(homePath);
+const trackPath = join(homePath, "ADA");
+console.log(trackPath);
+
+if (existsSync(trackPath)) {
+  console.log("oui");
+} else {
+  console.log("non");
+}
+
+// const data = readFileSync("track.json", "utf-8");
+// const parsedData = JSON.parse(data);
+// console.log(parsedData);
