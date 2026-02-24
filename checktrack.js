96f2ec30d1c07a9be4ec658b0f7a4f96d8a95fdb
Guewen <guewenmalary779@gmail.com>
2026-02-24 12:23:23 +0100

    dvrai etape 4

--- checktrack.js
+++ checktrack.js
@@ -4,7 +4,7 @@ import { join } from "path";
 
 const homePath = homedir();
 console.log(homePath);
-const trackPath = join(homePath, "ADA");
+const trackPath = join(homePath, "Documents", "ADA", "projet");
 console.log(trackPath);
 
 // const trackPath = join(homePath, "Documents", "ADA", "projet");
@@ -12,28 +12,35 @@ console.log(trackPath);
 
 const data = readFileSync("track.json", "utf-8");
 const parsedData = JSON.parse(data);
+console.log(parsedData.projects);
 
-const adaPath = join(homePath, "ADA");
+// const adaPath = join(homePath, "ADA");
 
-if (existsSync(adaPath)) {
-  console.log("✅ dossier ada");
-} else {
-  console.log("❌ dossier ada");
-}
+// if (existsSync(trackPath)) {
+//   console.log("✅ dossier ada");
+// } else {
+//   console.log("❌ dossier ada");
+// }
 
-for (const project of parsedData.projects) {
-  if (existsSync(homePath)) {
+const projects = parsedData.projects;
+for (const project of projects) {
+  const projectPath = join(trackPath, project.name);
+  console.log(project.name);
+  console.log(projectPath);
+
+  if (existsSync(projectPath)) {
     console.log("oui");
   } else {
     console.log("non");
-
-    const gitPath = join(projectPath, ".git");
-
-    if (!existsSync(gitPath)) {
-      console.log("git n'est pas initialisé");
-    } else {
-      console.log(`❌ dossier du projet ${project.name}`);
-      console.log("- le dossier n'existe pas ou n'est pas nommé correctement");
-    }
+    //   const gitPath = join(projectPath, ".git");
+    //   if (!existsSync(gitPath)) {
+    //     console.log("git n'est pas initialisé");
+    //   } else {
+    //     console.log(`❌ dossier du projet ${project.name}`);
+    //     console.log("- le dossier n'existe pas ou n'est pas nommé correctement");
+    //   }
+    // }
+    // }
+    // }
   }
 }
